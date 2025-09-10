63e0a18420aac75df6ab904b867166250e645f32
Prothom <prothomghosh41@gmail.com>
2025-09-10 17:56:38 +0600

    Data Model created, connection made and mailer created

---
+++ src/dbConnection/dbConnection.ts
@@ -0,0 +1,21 @@
+import mongoose, { connection } from "mongoose";
+
+export async function connect() {
+    try{
+        mongoose.connect(process.env.MONGO_URL!)
+        const connecton = mongoose.connection
+
+        connection.on('connected', () =>{
+            console.log('MongoDB connented');
+        })
+
+        connection.on('error', (err) => {
+            console.log("Mongodb connection error" + err);
+            process.exit()
+        })
+    }
+    catch(error){
+        console.log('Something went wrong connection to Databse');
+        console.log(error);
+    }
+}
---
+++ src/helpers/mailer.ts
@@ -0,0 +1,30 @@
+import nodemailer from 'nodemailer'
+
+export const sendEmail = async({email, emailType, userId}) =>
+{
+    try{
+        const transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 465,
+            secure: true,
+            auth: {
+                user: "maddison53@ethereal.email",
+                pass: "jn7jnAPss4f63QBp6D",
+            },
+        });
+
+        const mailOptions = {
+            from: 'prothomghosh41@gmail.com',
+            to: email,
+            subject: emailType === 'VERIFY' ? "Verify your email" : "Reset your Password",
+            html: "<b>Hello world?</b>", // HTML body
+        }
+
+        const mailResponse = await transporter.sendMail(mailOptions)
+        return mailResponse
+    }
+
+    catch(error:any){
+        throw new Error(error.message)
+    }
+}
