fe84362cd099b28f99894e8f90e81830afd44483
Prothom <prothomghosh41@gmail.com>
2025-09-27 17:07:43 +0600

    Front end started

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+export default function SignupPage(){
+
+    const [user, setUser] = useState({
+        email: "",
+        password: "",
+        username: ""
+    })
+
+    const [buttonDisabled, setButtonDisabled] = useState
+    (false)
+
+    const[loading, setLoading] = useState(false)
+
+    const onSignup = async() =>{
+        try{
+
+        }
+        catch(error : any)
+        {
+            console.log("Signup failed");
+        }
+    }
+    return(
+        <div>Sign up Pagr</div>
+    )
+}
+
