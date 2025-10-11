eb06bf51d61ee0f00c78ef425cc6ff5a7808f566
Prothom <prothomghosh41@gmail.com>
2025-10-11 11:02:00 +0600

    Header and Footer Added

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>© {new Date().getFullYear()} AirportName. All rights reserved.</p>
+      <p>Contact us: info@airportname.com | +1 (123) 456-7890</p>
+    </footer>
+  );
+};
+
+export default Footer;
